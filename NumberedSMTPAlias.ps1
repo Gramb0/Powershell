c9285e99d9fb415532ec7bdbf7ca6e192831cc89
Gramb0 <39170048+Gramb0@users.noreply.github.com>
2018-06-06 15:13:18 -0400

    Script for adding SMTP alias' to an account

---
+++ NumberedSMTPAlias.ps1
@@ -0,0 +1,11 @@
+﻿$startnumber=
+$endnumber=
+$mailboxusername=""
+$domain=""
+
+$i=$startnumber
+Do {
+    Set-RemoteMailbox "$mailboxusername" -EmailAddresses @{add="$mailboxusername$i@$domain"}
+    $i++
+    }
+Until ($i -eq $endnumber+1)
